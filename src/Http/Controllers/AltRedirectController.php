2b2bd2ee82c77c4d2cb9aab0d774e08b06193d36
benammi <benammi@alt-design.net>
2024-02-19 15:04:28 +0000

    Fixed an issue with DB based sites not having a blueprint directory

--- src/Http/Controllers/AltRedirectController.php
+++ src/Http/Controllers/AltRedirectController.php
@@ -32,7 +32,9 @@ class AltRedirectController
         $fields = $fields->preProcess();
 
         // Reset the directory to the old one
-        Blueprint::setDirectory($oldDirectory);
+        if ($oldDirectory) {
+            Blueprint::setDirectory($oldDirectory);
+        }
 
         return view('alt-redirect::index', [
             'blueprint' => $blueprint->toPublishArray(),
@@ -46,7 +48,7 @@ class AltRedirectController
     {
 
         // Grab the old directory just in case
-        $oldDirectory = Blueprint::directory();
+//        $oldDirectory = Blueprint::directory();
 
         $data = new Data('redirects');
 
@@ -68,7 +70,7 @@ class AltRedirectController
         $values = $data->all();
 
         // Reset the directory to the old one
-        Blueprint::setDirectory($oldDirectory);
+//        Blueprint::setDirectory($oldDirectory);
 
         return [
             'data' => $values
