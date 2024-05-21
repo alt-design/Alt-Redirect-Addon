0df64856eb6cca99d0aa3ae1a70430dc35e7676b
Marty Friedel <1491079+martyf@users.noreply.github.com>
2024-05-22 08:21:02 +0930

    Corrected grammar

--- config/alt-redirect.php
+++ config/alt-redirect.php
@@ -7,7 +7,7 @@ return [
     | Headers
     |--------------------------------------------------------------------------
     |
-    | A key/value pair of Headers to be included with the redirect. This
+    | A key/value pair array of Headers to be included with the redirect. This
     | applies to all redirects. For example:
     |
     | 'headers' => [
