f5567ad3f2fb6cc202c6310e92b04f3bb3351b8d
benammi <benammi@alt-design.net>
2025-02-04 12:08:00 +0000

    URISupport.php - fullUrlWithoutQuery() and myArrQuery() methods now support an optional $encoding_type param.
    The encoding type is chosen by testing the full request URI for a + and then choosing the appropriate encoding so redirects with either %20 or + space encoding is now supported.

--- src/Helpers/URISupport.php
+++ src/Helpers/URISupport.php
@@ -16,19 +16,29 @@ class URISupport
      */
     public static function uriWithFilteredQueryStrings() : string
     {
+        $request = request();
         $data = new Data('query-strings');
         $withoutQueryStrings = Arr::pluck($data->all(), 'query_string');
         // Filter out unwanted params, then strip the base url to get a filtered uri
-        return Str::replace(request()->root(), '', self::fullUrlWithoutQuery($withoutQueryStrings));
+        $encoding = str_contains($request->getRequestURI(), '+')
+            ? PHP_QUERY_RFC1738
+            : PHP_QUERY_RFC3986;
+
+        return Str::replace(
+            $request->root(),
+            '',
+            self::fullUrlWithoutQuery($withoutQueryStrings, $encoding)
+        );
     }
 
     /**
      * Replacement for fullUrlWithoutQuery() in Request to use custom Arr::query() implementation
      *
      * @param array $keys
+     * @param int $encoding_type (optional) Encoding Type
      * @return string
      */
-    private static function fullUrlWithoutQuery(array $keys) : string
+    private static function fullUrlWithoutQuery(array $keys, $encoding_type = PHP_QUERY_RFC1738) : string
     {
         $request = request();
         $query = Arr::except($request->query(), $keys);
@@ -36,7 +46,7 @@ class URISupport
         $question = $request->getBaseUrl().$request->getPathInfo() === '/' ? '/?' : '?';
 
         return count($query) > 0
-            ? $request->url().$question.self::myArrQuery($query)
+            ? $request->url().$question.self::myArrQuery($query, $encoding_type)
             : $request->url();
     }
 
@@ -44,10 +54,11 @@ class URISupport
      * Replacement for Arr::query() to use default encoding method
      *
      * @param array $array
+     * @param int $encoding_type (optional) Encoding Type
      * @return string
      */
-    private static function myArrQuery(array $array) : string
+    private static function myArrQuery(array $array, $encoding_type = PHP_QUERY_RFC1738) : string
     {
-        return http_build_query($array, '', '&');
+        return http_build_query($array, '', '&', $encoding_type);
     }
 }
